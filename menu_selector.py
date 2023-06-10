04ed9e3dc09c927ecc245c76ec7d5ae5a99a7e3e
Yujin Kim <jinx2196@gmail.com>
2023-06-10 22:30:26 +0900

    파일을 읽어서 데이터를 구성하고, 출력하는 기능 구현

--- menu_selector.py
+++ menu_selector.py
@@ -1,4 +1,24 @@
+'''
+사용자와 상호작용하기 위한 UI(User Interface)를 제공하는 모듈
+
+'''
+
+# file_manager와 parking_spot_manager을 import하여 사용
+import file_manager
+import parking_spot_manager
+
 def start_process(path):
+    """
+    프로그램의 실행 과정을 맡는 함수
+    Args:
+        path (string): 파일 경로
+    """
+
+    # file_manager 모듈의 read_file 함수를 호출하여 문자열 리스트를 반환
+    str_list = file_manager.read_file(path)
+    # parking_spot_manager 모듈의 str_list_to_class_list 함수로 parking_spot 객체의 리스트 반환
+    spots = parking_spot_manager.str_list_to_class_list(str_list)
+
     while True:
         print("---menu---")
         print("[1] print")
@@ -6,9 +26,11 @@ def start_process(path):
         print("[3] sort")
         print("[4] exit")
         select = int(input('type:'))
+        
+        #parking_spot_manager 모듈의 print_spots 함수 호출
         if select == 1:
-            print("not implemented yet")
-            # fill this block
+            parking_spot_manager.print_spots(spots)
+            
         elif select == 2:
             print("---filter by---")
             print("[1] name")
@@ -51,8 +73,10 @@ def start_process(path):
                 print("not implemented yet")
                 # fill this block
             else: print("invalid input")
+
+        #Exit 출력하고 반복 종료
         elif select == 4:
-            print("not implemented yet")
-            # fill this block
+            print("Exit") 
+            break
         else:
             print("invalid input")
--- parking_spot_manager.py
+++ parking_spot_manager.py
@@ -1,21 +1,62 @@
+'''
+무료주차장의 데이터를 분석하고 관리하기 위한 모듈
+'''
+
 class parking_spot:
-    # you have to implement 'constructor(생성자)' and 'get' method
+    
+    def __init__(self, name, city, district, ptype, longitude, latitude):
+        self.__item = {
+            'name': name,
+            'city': city,
+            'district': district,
+            'ptype': ptype,
+            'longitude': longitude,
+            'latitude': latitude
+        }
+
+
     def __str__(self):
         item = self.__item
         s  = f"[{item['name']}({item['ptype']})] "
         s += f"{item['city']} {item['district']}"
         s += f"(lat:{item['latitude']}, long:{item['longitude']})"
         return s
+    
+    def get(self, keyword='name'):
+        return self.__item[keyword]
+
+#문자열 리스트를 클래스 객체 리스트로 반환
+def str_list_to_class_list(str_list):
+    class_list = []
+    for item in str_list:
+        #쉼표를 기준으로 데이터 나누기
+        values = item.split(',')
+        name = values[1]
+        city = values[2]
+        district = values[3]
+        ptype = values[4]
+        longitude = float(values[5])
+        latitude = float(values[6])
+        spot = parking_spot(name, city, district, ptype, longitude, latitude)
+        #클래스 리스트에 추가하기
+        class_list.append(spot)
+    return class_list    
+
+#리스트에 저장된 모든 객체의 값 출력
+def print_spots(spots):
+    print(f"---print elements({len(spots)})---")
+    for spot in spots:
+        print(spot)
 
 
 # 각 단계별로 테스트 (테스트할때 주석해제 후 사용)
 if __name__ == '__main__':
     print("Testing the module...")
-    # version#2
-    # import file_manager
-    # str_list = file_manager.from_file("./input/free_parking_spot_seoul.csv")
-    # spots = str_list_to_class_list(str_list)
-    # print_spots(spots)
+    #version2
+    #import file_manager
+    #str_list = file_manager.from_file("./input/free_parking_spot_seoul.csv")
+    #spots = str_list_to_class_list(str_list)
+    #print_spots(spots)
 
     # version#3
     # spots = filter_by_district(spots, '동작')
